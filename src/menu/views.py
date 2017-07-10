fdbc0bd41c8995a1004bf33036c4271b1c816474
saadmk11 <saad.mk11@hotmail.com>
2017-07-10 20:55:56 +0600

    search function added

--- src/menu/views.py
+++ src/menu/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from .models import Categories, Menu
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -8,8 +9,17 @@ from django.shortcuts import render, get_object_or_404
 def menu_list(request): # shows list of menu items & categories
     query_list = Menu.objects.filter(available=True)
     category = Categories.objects.all()
+    query = request.GET.get("q")
+    
+    if query:
+        query_list = query_list.filter(
+            Q(item__icontains=query)|
+            Q(category__cat_name__icontains=query)
+            ).distinct()
+
     context = { 'query_list': query_list,
                 'category': category,
+                "title": "Items"
             }
 
     return render(request, 'menu/menu_list.html', context)
@@ -20,16 +30,36 @@ def menu_detail(request, slug=None):
 
     return render(request, 'menu/menu_detail.html', context)
 
-
 def cat_list(request): # shows list of categories
     cat_qs = Categories.objects.all()
-    context = { "cat_qs": cat_qs, }
+    query = request.GET.get("q")
+
+    if query:
+        cat_qs = cat_qs.filter(
+            Q(cat_name__icontains=query)
+            ).distinct()
+
+    context = { "cat_qs": cat_qs,
+                "title": "Categories"
+                 }
 
     return render(request, "menu/cat_list.html", context)
 
 def cat_detail(request, cat_name=None): # shows list of items in a category
     categories = get_object_or_404(Categories, cat_name__iexact=cat_name)
     menu_qs = categories.menu_set.filter(available=True)
-    context = { "menu_qs": menu_qs, }
+    query = request.GET.get("q")
+
+    if query:
+        menu_qs = menu_qs.filter(
+            Q(item__icontains=query)
+            ).distinct()
+
+    context = { "menu_qs": menu_qs,
+                "title": "Items"
+
+                 }
 
     return render(request, "menu/cat_detail.html", context)
+
+
--- src/restaurant/settings.py
+++ src/restaurant/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'menu',
     'index',
+    'contact',
 ]
 
 MIDDLEWARE = [
